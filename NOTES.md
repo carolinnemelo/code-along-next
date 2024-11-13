607dd6a48f651c596b0df3247374d9423915ff28
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-13 09:26:30 +0100

    create home for ic

---
+++ NOTES.md
@@ -0,0 +1,7 @@
+## Start with get all
+CRUD  
+first case so you can validate the next steps | getting an empty array.
+
+## Start with UI not UX 
+what data do I need on the screen? 
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 
 
-export default function Home() {
+export default async function Home() {
+  const iceCreams = await [];
+
   return (
-    <h1>Ice creams</h1>
+    <>
+      <h1>Ice cream land</h1>
+      {!iceCreams.length && (
+        <p>Ohh, you have no Ice Cream. We need to fix that, asap!</p>
+      )}
+    </>
   );
 }
